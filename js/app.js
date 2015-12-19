edb46b528a9317d6e96b158e797bc6a1cd4fa5af
Greta Kavaliauskaite <greta.kavaliauskaite@cohaesus.co.uk>
2015-12-19 15:06:36 +0000

    changes in app.js

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = canvas.width/2;
+    this.y = canvas.height/2;
 };
 
 // Update the enemy's position, required method for game
@@ -21,15 +23,55 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+
+var Player = function() {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/char-boy.png';
+    this.x = canvas.width/2;
+    this.y = canvas.height-50;
+
+};
+
 // Now write your own player class
 // This class requires an update(), render() and
+Player.prototype.update = function(dt) {
+    /*if (38 in keysDown){
+        player.y-=player.speed*dt;
+    }
+    if (40 in keysDown){
+        player.y+=player.speed*dt;
+    }
+    if (37 in keysDown){
+        player.x-=player.speed*dt;
+    }
+    if (39 in keysDown){
+        player.y+=player.speed*dt;
+    }
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+   // all computers.
+   */ 
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function() {
+    
+};
 // a handleInput() method.
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var allEnemies=[([Enemy])];
+var player= Player;
 
 
 // This listens for key presses and sends the keys to your
